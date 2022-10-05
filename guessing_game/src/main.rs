ace91e3c3402e2c27d795119a55df62b4f863518
Max <max@ref.finance>
2022-10-05 23:44:28 +0800

    update guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,38 @@
 use std::io;
 
+use rand::Rng; // trait
+
+use std::cmp::Ordering;
+
 fn main() {
     println!("guessing !");
 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1, 101); // i32 u32 i64
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // shadow
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
